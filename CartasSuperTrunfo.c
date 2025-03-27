eb41852f7f5bfce606793afd92d1e9c9f631ebcd
programmertiago <tiagofelipe.lopes@gmail.com>
2025-03-27 02:40:33 +0000

    PRINTF E SCANF

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,37 @@ int main() {
 
     printf("Desafio Super Trunfo!\n");
 
+    char codigo_da_cidade [50] = "H04";
+    char nome_da_cidade [50] = "Manaus";
+    int populacao = 1000;
+    float area = 1000.1;
+    float PIB = 1000.1;
+    int pontos_turisticos = 1;
+
+    printf("Digite o Código da Cidade: ");
+    scanf("%s", codigo_da_cidade);
+    printf("Código da cidade: %s\n", codigo_da_cidade);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s", nome_da_cidade);
+    printf("Nome da cidade: %s\n", nome_da_cidade);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao);
+    printf("População: %d\n", populacao);
+
+    printf("Digite a Área: ");
+    scanf("%f", &area),
+    printf("Área: %.2f\n", area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB),
+    printf("PIB: %.2f\n", PIB);
+
+    printf("Digite os Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos),
+    printf("Pontos Turísticos: %d\n", pontos_turisticos);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
